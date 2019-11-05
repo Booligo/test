19d8efb2c913e203ea129c70a5171ec2063bb1c0
Ivan <vanadublanin@gmail.com>
2019-11-05 21:09:25 +0300

    наш четвёртый коммит

--- hhhh.js
+++ hhhh.js
@@ -1,4 +1,30 @@
-'use strict'
-alert("Julia ya lublu vas ");
-var a=228;
-console.log(a);
+'use strict';
+ 
+    
+    let money = prompt("Ваш бюджет на месяц?");
+    let time = prompt("Введите дату в формате YYYY-MM-DD");
+    let 
+    a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется?");
+    let appData = {
+        budzhet: money,
+        timeData: time,
+        income:[],
+        expenses:{
+        a1: a2,
+        a3: a4
+           
+        },
+        optionalExpenses:{} , 
+        savings: false
+    };
+   // let 
+   /*a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется?");*/
+    appData.expenses.a1 = a2;
+    appData.expenses.a3 = a4;
+    alert(appData.budzhet/30);
